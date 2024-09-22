d094adda1b945c5fa213708de0be0088f3221f29
KaterinaLaz <p18laza@ionio.gr>
2024-09-22 16:40:18 +0300

    update firebaseConfing

--- src/pages/FirebaseConfig.ts
+++ src/pages/FirebaseConfig.ts
@@ -7,18 +7,17 @@ import { initializeApp } from 'firebase/app'
 import {getAuth} from 'firebase/auth'
 import { getDatabase } from '@firebase/database'
 import { getFirestore } from "firebase/firestore"
-import { getMessaging, getToken } from "firebase/messaging"
 
 
 const config = {
-    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-    databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
-    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-    appId: import.meta.env.VITE_FIREBASE_APP_ID,
-    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.VITE_FIREBASE_DATABASE_URL,
+    projectId: process.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.VITE_FIREBASE_APP_ID,
+    measurementId: process.env.VITE_FIREBASE_MEASUREMENT_ID,
    
   };
   
@@ -27,5 +26,5 @@ const app = initializeApp(config)
 export const auth = getAuth(app)
 export const database = getDatabase(app)
 export const dataFire = getFirestore(app)
-const messaging = getMessaging(app)
+
 
